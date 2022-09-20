74281e6b9bc71073b859954b528712db2be9c8e5
wishba <wisnubayu34@gmail.com>
2022-09-20 14:37:09 +0700

    nav menu button

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function showNav() {
   const navLink = document.getElementById("navLink");
   navLink.className = "nav__link nav__link-opened";
+  const navBtn = document.getElementById("navBtn");
+  navBtn.className = "nav__button nav__button-hide";
 }
 
 function closeNav() {
   const navLink = document.getElementById("navLink");
   navLink.className = "nav__link";
+  const navBtn = document.getElementById("navBtn");
+  navBtn.className = "nav__button";
 }
